bcdd2f915e06e3cc77d3c7c801eca572efb5d898
Bonta Vlad <bonta.vlad@gmail.com>
2017-11-02 12:25:35 +0200

    Another day in paradise.

--- lab3/lab3_2_b.pl
+++ lab3/lab3_2_b.pl
@@ -1,4 +1,29 @@
-%% For a heterogeneous list, formed from integer numbers and list of numbers,
-%% write a predicate to sort every sublist, keeping the doubles.
+%% For a heterogeneous list, formed from integer numbers and list of numbers, write a predicate to sort every sublist with removing the doubles.
 %% Eg.: [1, 2, [4, 1, 4], 3, 6, [7, 10, 1, 3, 9], 5, [1, 1, 1], 7] =>
-%% [1, 2, [1, 4, 4], 3, 6, [1, 3, 7, 9, 10], 5, [1, 1, 1], 7].
+%% [1, 2, [1, 4], 3, 6, [1, 3, 7, 9, 10], 5, [1], 7]
+
+
+apartine(X, [X|_]).
+apartine(X, [_|Rest]):- apartine(X, Rest).
+
+elim([], []):- !.
+
+elim([H|T], R):-
+    apartine(H, T),
+    elim(T, R).
+
+elim([H|T], [H|R]):- elim(T, R).
+
+elim_list([], []):- !.
+
+elim_list([H|T], R):-
+    is_list(H),
+    elim_list(H, R),
+    elim_list(T, R).
+
+elim_list([H|T], R):-
+    apartine(H, T),
+    elim_list(T, R).
+
+elim_list([H|T], [H|R]):-
+    elim_list(T, R).
